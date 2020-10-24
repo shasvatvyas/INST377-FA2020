d7cd8f7f14ab6566e812156afe502d1ba9f69635
shasvatvyas <68927406+shasvatvyas@users.noreply.github.com>
2020-10-23 23:47:23 -0400

    Lab 6 completed

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -1,4 +1,7 @@
 // You may wish to find an effective randomizer function on MDN.
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * Math.floor(max) - Math.floor(min));
+}
 
 function range(int) {
   const arr = [];
@@ -29,8 +32,31 @@ document.body.addEventListener('submit', async (e) => {
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
+      if(document.querySelector('.flex-inner')){
+        document.querySelector('.flex-inner').remove();
+      }
       // You're going to do your lab work in here. Replace this comment.
       console.log('fromServer', fromServer);
+      const newArray = range(10);
+      const newArray2 = newArray.map(() => {
+        const number = getRandomInt(0,243);
+        return fromServer[number];
+
+      });
+
+      const reverseList = newArray2.sort((a, b) => sortFunction(b, a, 'name'));
+      const ul = document.createElement('ul');
+      ul.className = 'flex-inner';
+      $('form').prepend(ul);
+
+      reverseList.forEach((el, i) => {
+        const li = document.createElement('li');
+        $(li).append(`<input type="checkbox" value = ${el.code} id = ${el.code} />`);
+        $(li).append(`<label for=${el.code}>${el.name}</label>`);
+        $(ul).append(li);
+
+      });
+    
     })
     .catch((err) => console.log(err));
 });
